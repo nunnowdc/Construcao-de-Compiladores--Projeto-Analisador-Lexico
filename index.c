83611d1a676ec904dab9757148a935cf40a6e5b2
Nunno Wakiyama Diniz Carvalho <142551010+nunnowdc@users.noreply.github.com>
2025-05-09 16:37:20 -0300

    Update index.c

--- index.c
+++ index.c
@@ -3,6 +3,6 @@ if (4.13 < 234.2 > 123)
 < auau >
 int main() {
     || | && & 
-    10 20 10 xixi coco xixi 10.2 20.5 10.2 "oi" "tchau" "oi" == != ==
+    10 20 10 pipi popo pipi 10.2 20.5 10.2 "hello" "bye" "hello" == != ==
     return 0;
 }
